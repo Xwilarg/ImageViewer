94fc08bc4fa6f83210bd97130a8697f1e5b1f512
Xwilarg <xwilarg@yahoo.fr>
2021-08-11 16:16:14 +0200

    Clean code

--- js/displayInfo.js
+++ js/displayInfo.js
@@ -1,54 +1,50 @@
-let availableFolders = [];
-
-let http = new XMLHttpRequest(); // Get all available folders
-http.open("GET", "php/getAllFolders.php", true);
-http.onreadystatechange = function ()
+async function updateScreenAsync()
 {
-    if (this.readyState === 4 && this.status === 200) {
-        JSON.parse(this.responseText).forEach(e => {
-            availableFolders.push(e);
-        });
-        availableFolders.sort();
+    let availableFolders = [];
 
-        let dir = new URL(window.location.href).searchParams.get("dir");
+    const resp = await fetch("php/getAllFolders.php");
+    const folders = await resp.json();
 
-        if (dir === null || !availableFolders.includes(dir)) // No dir parameter or an invalid one
-        {
-            let str = "";
-            availableFolders.forEach(e => {
-                str += '<a href="' + window.location.origin + '?dir=' + e + '">' + e + '</a><br/>';
-            });
-            document.getElementById("menu").innerHTML = str;
-        }
-        else
-        {
-            http = new XMLHttpRequest(); // Get all available files
-            http.open("GET", "php/getAllImages.php?folder=" + dir, true);
-            http.onreadystatechange = function ()
-            {
-                if (this.readyState === 4 && this.status === 200) {
-                    let images = JSON.parse(this.responseText);
-                    http = new XMLHttpRequest(); // Get all available files
-                    http.open("GET", "php/getInfoJson.php?folder=" + dir, true);
-                    http.onreadystatechange = function ()
-                    {
-                        if (this.readyState === 4 && this.status === 200) {
-                            let tags = JSON.parse(this.responseText);
-                            let str = "";
-                            images.forEach(e => {
-                                let filename = e.split('.')[0];
-                                let isNsfw = tags["nsfw"].includes(filename);
-                                let image = window.location.origin + '/data/' + dir + '/' + e;
-                                str += '<a data-lightbox="viewer" href="' + image + '"><img ' + (isNsfw ? 'class="nsfw"' : '') + ' src="' + image + '"/></a>';
-                            });
-                            document.getElementById("menu").innerHTML = str;
-                        }
-                    };
-                    http.send(null);
-                }
-            };
-            http.send(null);
-        }
-    }
-};
-http.send(null);
+    folders.forEach(e => {
+        availableFolders.push(e);
+    });
+    availableFolders.sort();
+
+    let dir = new URL(window.location.href).searchParams.get("dir");
+
+    let html = dir === null || !availableFolders.includes(dir) ? getImageFolders(availableFolders) : await getImagesAsync(dir);
+    document.getElementById("menu").innerHTML = html;
+}
+
+/// Get all the folders
+function getImageFolders(availableFolders)
+{
+    let str = "";
+    availableFolders.forEach(e => {
+        str += '<a href="' + window.location.origin + '?dir=' + e + '">' + e + '</a><br/>';
+    });
+    return str;
+}
+
+/// Get all the images inside a folder
+async function getImagesAsync(dir)
+{
+    // Get all images
+    let resp = await fetch("php/getAllImages.php?folder=" + dir);
+    const images = await resp.json();
+
+    // Get information JSON
+    resp = await fetch("php/getInfoJson.php?folder=" + dir);
+    const tags = await resp.json();
+
+    let str = "";
+    images.forEach(e => {
+        let filename = e.split('.')[0];
+        let isNsfw = tags["nsfw"].includes(filename);
+        let image = window.location.origin + '/data/' + dir + '/' + e;
+        str += '<a data-lightbox="viewer" href="' + image + '"><img ' + (isNsfw ? 'class="nsfw"' : '') + ' src="' + image + '"/></a>';
+    });
+    return str;
+}
+
+updateScreenAsync();
